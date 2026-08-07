a1728a98d09e262550d2fdc9d9bc5667dc3889b3
abdul kayyum <abdulkayyum20006@gmail.com>
2026-08-07 23:56:14 +0530

    feat(frontend): setup React Router and project structure

---
+++ frontend/src/pages/home/HomePage.tsx
@@ -0,0 +1,15 @@
+function HomePage() {
+  return (
+    <div className="min-h-screen bg-slate-950 text-white">
+      <h1 className="text-6xl font-bold text-center pt-40">
+        Welcome to SupportAI 🚀
+      </h1>
+
+      <p className="text-center text-slate-400 mt-6 text-xl">
+        AI Customer Support Platform
+      </p>
+    </div>
+  );
+}
+
+export default HomePage;
